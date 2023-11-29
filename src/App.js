2a51d1cca60a6b24912eba336d3842fbd95e1ed6
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-11-29 02:51:06 +0000

    UPDATE: static data to reusable mapped content

--- src/App.js
+++ src/App.js
@@ -1,50 +1,51 @@
 import "./styles/styles.scss"
 
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Soup"
+    },
+    {
+      id: 2,
+      title: "Salad"
+    },
+    {
+      id: 3,
+      title: "Pizza"
+    },
+    {
+      id: 4,
+      title: "Pasta"
+    },
+    {
+      id: 5,
+      title: "Curry"
+    },
+    {
+      id: 6,
+      title: "Deserts"
+    },
+    {
+      id: 7,
+      title: "Drinks"
+    }
+  ]
+
   return (
     <div className="wrapper">
-      <div className="container">
-        <div className="content">
-          <h2>Soup</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <h2>Salad</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <h2>Pizza</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <h2>Pasta</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <h2>Curry</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <h2>Deserts</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <h2>Drinks</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      {
+        categories.map(({title}) => (
+          <div className="container">
+            <div className="content">
+              <h2>
+                {title}
+              </h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        ))
+      }
     </div>
   )
 }
