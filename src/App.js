d28246f4633a05d654fe50471a240ac24d7910e7
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-11-29 03:29:33 +0000

    UPDATE: route so there are nested routes within

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import { Routes, Route } from "react-router-dom"
+import { Outlet, Route, Routes } from "react-router-dom"
 
 import Home from "./routes/Home/Home"
 
 import "./styles/styles.scss"
 
+const Navigation = () => {
+  return (
+    <>
+      <div>
+        navigation bar
+      </div>
+      <Outlet />
+    </>
+  )
+}
+
+const Shop = () => {
+  return (
+    <div>I am shop page</div>
+  )
+}
+
 const App = () => {
   return (
     <div className="app">
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Home />
-          }
-        />
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />}  />
+          <Route path="/shop" element={<Shop />} />
+        </Route>
       </Routes>
     </div>
   )
