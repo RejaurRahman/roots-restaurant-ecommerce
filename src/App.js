8467407d6809feaef98de8f7302bcd8219afd558
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-11-29 02:47:38 +0000

    UPDATE: markup for homepage

--- src/App.js
+++ src/App.js
@@ -2,10 +2,51 @@ import "./styles/styles.scss"
 
 const App = () => {
   return (
-    <div className="app">
-      Hello World
+    <div className="wrapper">
+      <div className="container">
+        <div className="content">
+          <h2>Soup</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <h2>Salad</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <h2>Pizza</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <h2>Pasta</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <h2>Curry</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <h2>Deserts</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <h2>Drinks</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App
