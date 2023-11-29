4e265f5697c7b4adade4c4b462ba6bf45dd9c6bb
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-11-29 03:11:34 +0000

    ADD: category item componentt

--- src/App.js
+++ src/App.js
@@ -1,49 +1,55 @@
+import CategoryItem from "./components/CategoryItem/CategoryItem"
+
 import "./styles/styles.scss"
 
 const App = () => {
   const categories = [
     {
       id: 1,
-      title: "Soup"
+      title: "Soup",
+      imageUrl: "https://www.foodiesfeed.com/wp-content/uploads/2015/05/autumn-pumpkin-soup-3.jpg"
     },
     {
       id: 2,
-      title: "Salad"
+      title: "Salad",
+      imageUrl: "https://www.foodiesfeed.com/wp-content/uploads/2016/04/salad-with-olives.jpg"
     },
     {
       id: 3,
-      title: "Pizza"
+      title: "Fast Food",
+      imageUrl: "https://www.foodiesfeed.com/wp-content/uploads/2016/09/burger-with-cheddar-bacon-and-guacamole.jpg"
     },
     {
       id: 4,
-      title: "Pasta"
+      title: "Oriental",
+      imageUrl: "https://www.foodiesfeed.com/wp-content/uploads/2018/08/vibrant-shot-of-feasting-on-chinese-steamed-and-fried-dim-sum.jpg"
     },
     {
       id: 5,
-      title: "Curry"
+      title: "Indian",
+      imageUrl: "https://www.foodiesfeed.com/wp-content/uploads/2023/03/close-up-of-butter-chicken-indian-dish.jpg"
     },
     {
       id: 6,
-      title: "Deserts"
+      title: "Desert",
+      imageUrl: "https://www.foodiesfeed.com/wp-content/uploads/2019/09/traditional-malaysia-deserts.jpg"
     },
     {
       id: 7,
-      title: "Drinks"
+      title: "Drinks",
+      imageUrl: "https://www.foodiesfeed.com/wp-content/uploads/2019/10/bartender-licor-beirao.jpg"
     }
   ]
 
   return (
-    <div className="wrapper">
+    <div className="app">
       {
-        categories.map(({title}) => (
-          <div className="container">
-            <div className="content">
-              <h2>
-                {title}
-              </h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
+        categories.map(({ id, title, imageUrl }) => (
+          <CategoryItem
+            key={id}
+            imageUrl={imageUrl}
+            title={title}
+          />
         ))
       }
     </div>
