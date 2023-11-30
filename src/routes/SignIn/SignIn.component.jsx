d285b760dc3ffec69db3d95b1c5c0edb29ab7a57
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-11-30 00:01:02 +0000

    ADD: firebase config to set up sign in with google

--- src/routes/SignIn/SignIn.component.jsx
+++ src/routes/SignIn/SignIn.component.jsx
@@ -1,8 +1,21 @@
 import React from "react"
 
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils"
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup()
+    console.log(response)
+  }
+
   return (
-    <div>sign in page</div>
+    <div>
+      sign in page
+
+      <button onClick={logGoogleUser}>
+        sign in with Google
+      </button>
+    </div>
   )
 }
 
