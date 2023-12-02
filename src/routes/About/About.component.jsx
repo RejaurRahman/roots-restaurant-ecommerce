1e4277b0c9dfd910927d992c6329d811073453f9
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-12-02 18:46:19 +0000

    ADD: text video component

--- src/routes/About/About.component.jsx
+++ src/routes/About/About.component.jsx
@@ -4,12 +4,14 @@ import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs.component"
 import FoundersList from "../../components/FoundersList/FoundersList.component"
 import PageBanner from "../../components/PageBanner/PageBanner.component"
 import TextMedia from "../../components/TextMedia/TextMedia.component"
+import TextVideo from "../../components/TextVideo/TextVideo.component"
 
 const About = () => {
   return (
     <>
       <PageBanner label="About" />
       <Breadcrumbs label="About Us" />
+      <TextVideo />
       <FoundersList />
       <TextMedia />
     </>
--- src/routes/Navigation/MenuList/MenuList.component.jsx
+++ src/routes/Navigation/MenuList/MenuList.component.jsx
@@ -45,14 +45,14 @@ const MenuList = ({ showModal }) => {
         <li className="nav-item">
           {currentUser ? (
             <span
-              className="nav-link navbar--link"
+              className="nav-link navbar-link"
               onClick={signOutUser}
             >
               Sign Out
             </span>
           ) : (
             <Link
-              className="nav-link navbar--link"
+              className="nav-link navbar-link"
               to="/auth"
             >
               Sign In
