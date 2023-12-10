5ed6c6c3b9aeb4ac210a529012fc5e6f84d85840
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-12-10 20:04:53 +0000

    ADD: product ingredients modal

--- src/components/ProductCard/ProductCard.component.jsx
+++ src/components/ProductCard/ProductCard.component.jsx
@@ -1,17 +1,20 @@
-import React, { useContext } from "react"
+import React, { useState, useContext } from "react"
 import { LazyLoadImage } from "react-lazy-load-image-component"
 
 import { CartContext } from "../../contexts/cart.context"
 
 import Button from "../Button/Button.component"
+import ProductModal from "./ProductModal/ProductModal.component"
 
 import "./ProductCard.styles.scss"
 
 const ProductCard = ({ product }) => {
+  const [showModal, setShowModal] = useState(false)
   const {
     calories,
     diet,
     imageUrl,
+    ingredients,
     name,
     notes,
     price
@@ -22,6 +25,22 @@ const ProductCard = ({ product }) => {
   const addProductToCart = () => addItemToCart(product)
 
   const formattedPrice = `£${price.toFixed(2)}`
+
+  const toggleModal = () => {
+    setShowModal(!showModal)
+
+    if (!showModal) {
+      document.body.style.overflow = "hidden"
+    } else {
+      document.body.style.overflow = "auto"
+    }
+  }
+
+  const closeModal = () => {
+    setShowModal(false)
+
+    document.body.style.overflow = "auto"
+  }
 
   return (
     <div className="product-card-container col-12 col-md-6 col-lg-6 col-xl-4">
@@ -41,10 +60,20 @@ const ProductCard = ({ product }) => {
             </span>
           </div>
           <button
+            aria-label="Toggle product ingredients"
             className="product-item-button"
+            onClick={toggleModal}
+            type="button"
           >
             i
           </button>
+          {showModal && (
+            <ProductModal
+              closeModal={closeModal}
+              ingredients={ingredients}
+              showModal={showModal}
+            />
+          )}
         </div>
         {diet && diet.length > 0 && (
           <ul className="vegan-veg">
---
+++ src/components/ProductCard/ProductModal/ProductModal.component.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+import { createPortal } from "react-dom"
+
+import "./ProductModal.styles.scss"
+
+const ProductModal = ({
+  closeModal,
+  ingredients,
+  showModal
+}) => {
+  return (
+    showModal && createPortal(
+      <div
+        aria-hidden={!showModal}
+        aria-labelledby="navigationModalLabel"
+        className={`modal fade product-modal ${showModal ? "show" : ""}`.trim()}
+        onClick={closeModal}
+        tabIndex="-1"
+      >
+        <div className="modal-overlay" />
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <h3 className="product-modal-heading">Ingredients</h3>
+            <ul className="product-modal-list">
+              {ingredients.map((ingredient, index) => (
+                <li
+                  className="product-modal-listItem"
+                  key={index}
+                >
+                  {ingredient}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>,
+      document.getElementById("modal-root")
+    )
+  )
+}
+
+export default ProductModal
