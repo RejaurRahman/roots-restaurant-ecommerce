1aa885e0ff8e458d1c6925e8a9cc90b6f3fac41a
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-12-11 01:05:34 +0000

    ADD: redux persist library

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,41 @@
 import {
-  applyMiddleware,
   compose,
-  configureStore
+  configureStore,
+  applyMiddleware
 } from "redux"
+import {
+  persistReducer,
+  persistStore
+} from "redux-persist"
+import storage from "redux-persist/lib/storage"
 
+// import logger from "redux-logger";
 import { rootReducer } from "./root-reducer"
 
 const loggerMiddleware = (store) => (next) => (action) => {
   if (!action.type) {
-    return next(action)
+    return next(action);
   }
 
   next(action)
 }
 
-const middlewares = [loggerMiddleware]
+const middleWares = [loggerMiddleware]
 
-const composedEnhancers = compose(applyMiddleware(...middlewares))
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"]
+}
 
-export const store = configureStore(rootReducer, undefined, composedEnhancers)
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const composedEnhancers = compose(applyMiddleware(...middleWares))
+
+export const store = configureStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+)
+
+export const persistor = persistStore(store)
